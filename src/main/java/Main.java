f9666a76d30de910e535612882c773fd3ac0d847
Диана Бережная <Lacshri@yandex.ru>
2022-04-19 15:38:57 +0300

    assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
         long amount = 1000_60;
         boolean registered = true;
+        long expected = 30;
 
-        service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    private static void assertEquals(long expected, long actual) {
     }
 }
-
